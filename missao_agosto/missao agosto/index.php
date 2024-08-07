a4197d4813164c6e6d728aba2f0314f2b9158dda
Delmondes34 <gustavodel04@gmail.com>
2024-08-07 15:55:31 -0300

    atualização
    como fazer o banco funcionar que raivaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!!!

--- missao_agosto/missao agosto/index.php
+++ missao_agosto/missao agosto/index.php
@@ -1,10 +1,8 @@
-<?php 
-require_once 'coneção.php'
-
-?>
-
+<?php
+ include_once 'pasta_para_conectar/conexao.php'; 
+ ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt_br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,39 +11,44 @@ require_once 'coneção.php'
 </head>
 <body>
     <div>
-        <h1>Bem vindo a Redação</h1>
+        <h1>Bem vindo a escrivaninha </h1>
         <h4>seu melhor editor de historia</h4>
     </div>
-    <section>
-    <form action="pagina.php" method="get" >
-        <div class="col-md-4">
-            <div>
-                <label for="validationCustom01" class="form-label">Digite o nome da historia</label>
-                <input type="text" require class="form-control" id="validationCustom01"  aria-describedby="emailHelp" name="nome_da_historia" required>
-            </div>
-            <div>
-                <label for="">Digite o genero</label>
-                <select name="genero"  class="form-select" aria-label="Default select" id="genero" required>
-                        <option value=""></option>
-                        <option value="Ação">Ação</option>
-                        <option value="Comedia">Comedia</option>
-                        <option value="Romance">Romance</option>
-                        <option value="Terro">Terro</option>
-                        <option value="Drama">Drama</option>
-                        <option value="Ficção">Ficção</option>
-                    </select>
-            </div>
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label"> Comece a digitar a sua historia</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="4" name="historia" required></textarea>
-        </div>
-                    <a href="pagina.php"><button onclick="confirmar(); return false" class="btn btn-success form-control">confirmar</button> </a>
-            </div>        
-        </form> 
-     
-       
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></Script>
+    <br>
+    <h4>aqui esta alguma de nossas historias</h4>
+    <p>elas seram incluidas na tabela abaixo </p>
+    <table class="table table-hover table-bordered">
+        <thead>
+            <tr>
+                <td>Nome da historia</td>
+                <td>Genero</td>
+                <td>Historia</td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <?php 
+                    $sql = 'SELECT * FROM historia';
+                    $result = mysqli_query($conectar , $sql);
+                    while($dados = mysqli_fetch_array($resul)):
+                ?>
+                <td><?php echo $dados['nome_da_historia'];?></td>
+                <td><?php echo $dados['genero'];?></td>
+                <td><?php echo $dados['historia'];?></td>
+                <td>
+                    <a href="alteracao.php"><button>altera </button></a>
+                    <a><button>excluir</button></a>
+                </td>
+            </tr>
+            <?php endwhile; ?>    
+        </tbody>
+    </table>
+    <p>se quiser escreva as suas historias, clicar nesse botão nesse botão</p>
+    
+    <a href="Escritor.php" class="form-label"><button>escrever outra historia</button></a>
+    <button>jj</button>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ missao_agosto/missao agosto/pasta_para_conectar/conexao.php
@@ -0,0 +1,18 @@
+<?php 
+
+    $localDeAcesso = "localhost";
+    $bancoDeDados="crud";
+    $usuario = "root";
+    $senha= "";
+
+    $conectar = new mysqli($localDeAcesso, $usuario, $senha, $bancoDeDados );
+
+    if($conectar -> connect_errno){
+        echo "deu errado:(" . $conectar->connect_errno.")".$conectar->connect_errno;
+    }
+    else{
+        echo "Deu certo";
+    }
+
+    
+?>
